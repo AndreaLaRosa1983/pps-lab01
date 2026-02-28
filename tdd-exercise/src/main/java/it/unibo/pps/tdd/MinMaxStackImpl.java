6981b3c6c78909a704656e9def4f24aeb82e09ae
Andrea La Rosa <andrea.larosa2@studio.unibo.it>
2026-02-28 17:23:47 +0100

    MinMasStack size after push test RED + isEmpty Implementation GREEN

--- tdd-exercise/src/main/java/it/unibo/pps/tdd/MinMaxStackImpl.java
+++ tdd-exercise/src/main/java/it/unibo/pps/tdd/MinMaxStackImpl.java
@@ -1,4 +1,47 @@
 package it.unibo.pps.tdd;
 
-public class MinMaxStackImpl {
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MinMaxStackImpl implements MinMaxStack {
+
+
+    private final Deque<Integer> stack = new ArrayDeque<>();
+
+
+    @Override
+    public void push(int value) {
+
+    }
+
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public int peek() {
+        return 0;
+    }
+
+    @Override
+    public int getMin() {
+        return 0;
+    }
+
+    @Override
+    public int getMax() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
--- tdd-exercise/src/test/java/it/unibo/pps/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/it/unibo/pps/tdd/MinMaxStackImplTest.java
@@ -1,12 +1,29 @@
 package it.unibo.pps.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
+
+
+    private MinMaxStack stack;
+
+    @BeforeEach
+    void setUp() {
+        stack = new MinMaxStackImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    void newStackIsEmpty() {
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    void stackIsNotEmptyAfterPush() {
+        stack.push(10);
+        assertFalse(stack.isEmpty());
+        assertEquals(1, stack.size());
     }
 }
